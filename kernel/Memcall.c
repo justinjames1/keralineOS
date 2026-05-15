18afcf494f8a7f25c39ebfb9983169af31614c3d
justindude057-collab <justindude057@gmail.com>
2026-05-14 22:17:33 -0400

    Update Memcall.c

--- kernel/Memcall.c
+++ kernel/Memcall.c
@@ -1,12 +1,9 @@
 #ifndef memcall_H  
 #define memcall_H
-int main(){
 int memval; 
 void memcall()
   {int memory[memval];}
 int pid = 0;
 void pidcall(){
 pid ++; } // process adresess handling
-
-return 0;}
 #endif                
