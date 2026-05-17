b9c71d085967cf0df6f873cced2e253a051ae520
justindude057-collab <justindude057@gmail.com>
2026-05-17 00:34:20 -0400

    Update BOOTSECTION.asm

--- BOOTSECTION.asm
+++ BOOTSECTION.asm
@@ -51,9 +51,61 @@ int 0x10
 .line_loop:        
     lodsb         
     or al, al       
-    jz halt         
+    jz next2         
     int 0x10        
     jmp .line_loop 
+
+next2:
+
+    mov si, driveA       
+    mov ah, 0x0e       
+
+mov al, 0x0D
+int 0x10
+mov al, 0x0A
+int 0x10
+
+.driveA_loop:        
+    lodsb         
+    or al, al       
+    jz next3       
+    int 0x10        
+    jmp .driveA_loop 
+
+next3:
+
+    mov si, driveB      
+    mov ah, 0x0e       
+
+mov al, 0x0D
+int 0x10
+mov al, 0x0A
+int 0x10
+
+.driveB_loop:        
+    lodsb         
+    or al, al       
+    jz next4         
+    int 0x10        
+    jmp .driveB_loop 
+
+next4:
+
+    mov si, driveC     
+    mov ah, 0x0e       
+
+mov al, 0x0D
+int 0x10
+mov al, 0x0A
+int 0x10
+
+.driveC_loop:        
+    lodsb         
+    or al, al       
+    jz halt         
+    int 0x10        
+    jmp .driveC_loop 
+
 
 halt:
 hlt
@@ -61,8 +113,9 @@ nohlt:
 title db "Kernaline os bootloader.", 0
 select db "select a drive..", 0
 line db "================", 0
-
-
+driveA db "drive A:", 0
+driveB db "drive B:", 0
+driveC db "drive C:", 0
 
 Times 510 db 0
 db 0x55, 0xaa 
