fe58a98d72937e6dc779bf3f11b2238fb6a4cae2
Justin <justindude057@gmail.com>
2026-05-24 12:19:38 -0400

    Update Process_scheduler.c

--- kernel/Process_scheduler.c
+++ kernel/Process_scheduler.c
@@ -1,10 +1,13 @@
-#include "Memcall.h"
-int main(){
-int memval = 4096;
-Memcall();
-int pid = 0;
-void PID()
-{pid ++;}
-void PIDRET()
-{pid --;}
-return 0;}
+//#include "Memcall.h"
+//int main(){
+//int memval = 4096;
+//Memcall();
+//int pid = 0;
+//void PID()
+//{pid ++;}
+//void PIDRET()
+//{pid --;}
+//return 0;}
+
+
+//compiler was being shit so i noted the code. it was a linker error btw.
