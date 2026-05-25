c5a478d577c05a9062a6779593ff3646176f28df
Justin <justindude057@gmail.com>
2026-05-24 20:51:21 -0400

    Update Process_scheduler.c

--- kernel/Process_scheduler.c
+++ kernel/Process_scheduler.c
@@ -1,14 +1,12 @@
-//#include "Memcall.h"
-//int main(){
-//int memval = 4096;
-//Memcall();
-//int pid = 0;
-//void PID()
-//{pid ++;}
-//void PIDRET()
-//{pid --;}
-//return 0;}
+#include "Memcall.h"
+int main(){
+Memcall();
+int pid = 0;
+void PID()
+{pid ++;}
+void PIDRET()
+{pid --;}
+return 0;}
 
-int main()
-{} // so compile doesnt scream that theres no defined main.
+ // so compile doesnt scream that theres no defined main.
 //compiler was being shit so i noted the code. it was a linker error btw.
