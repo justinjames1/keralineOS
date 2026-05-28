2679675cc2dc991aebb3850dd57a1c63168c5d89
Wasgyst <ohsatanalimbrb@gmail.com>
2026-05-28 18:37:00 +0500

    Update Kernel_jmp.c

--- bootloader/Kernel_jmp.c
+++ bootloader/Kernel_jmp.c
@@ -1,3 +1,4 @@
 int main(){
-// someone add the fucking jump code here.
+// someone add the fucking jump code here. -Justinjames1
+// YOU COULD LITERALLY PROGRAMMED THE JUMP CODE IN ASSEMBLY, HOW IS IT HARD FOR YOU? and also idk where do you load kernel in... -Wasgyst
 }
