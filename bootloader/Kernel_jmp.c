078433b1f28a3658e74cb6a56916de9712edb1e8
Justin <justindude057@gmail.com>
2026-06-02 01:10:44 -0400

    Delete bootloader/Kernel_jmp.c

--- bootloader/Kernel_jmp.c
+++
@@ -1,4 +0,0 @@
-int main(){
-// someone add the fucking jump code here. -Justinjames1
-// YOU COULD LITERALLY PROGRAMMED THE JUMP CODE IN ASSEMBLY, HOW IS IT HARD FOR YOU? and also idk where do you load kernel in... -Wasgyst
-}
