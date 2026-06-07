c5411fc18fe4b35c66903db0f17dbf12b449d741
Justin <justindude057@gmail.com>
2026-06-07 00:17:03 -0400

    Update and rename kernel_panic.rs to kernel_panic.c

---
+++ kernel/Panic/kernel_panic.c
@@ -0,0 +1,25 @@
+//keralineOS (2026) (distrubtion of this code is allowed.)
+#include "krnlcall.h" //so we can collect data from krnlcall.
+
+int main(){
+ //get register values for diagonostics
+int rax_val = 0;
+_asm_("mov [rax_val], rax");
+int rbx_val = 0;
+_asm_("mov [rbx_val], rbx");
+int rcx_val = 0;
+_asm_("mov [rcx_val], rcx"); 
+int rdx_val = 0;
+_asm_("mov [rdx_val], rdx");
+int rsi_val = 0;
+_asm_("mov [rsi_val], rsi");
+int rdi_val = 0;
+_asm_("mov [rdi_val], rdi");
+int rbp_val = 0;
+_asm_("mov [rbp_val], rbp");
+int rsp_val = 0;
+_asm_("mov [rsp_val], rsp");
+
+
+ 
+}
--- kernel/Panic/kernel_panic.rs
+++
@@ -1,4 +0,0 @@
-fn main(){
-print!("KERNEL PANIC. ERRROR CODE [");
- println!("Oxbeta]");
-}
