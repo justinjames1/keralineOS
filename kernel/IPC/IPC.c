819419bd2d67ed1a5c3db9f2eae5ce28c629d864
Justin <justindude057@gmail.com>
2026-06-08 02:15:16 -0400

    Update IPC.c

--- kernel/IPC/IPC.c
+++ kernel/IPC/IPC.c
@@ -1,8 +1,25 @@
 #include "Memory/Memcall.h"
 int main()
 {
-int shared_memory_amount = 450;
 
+int base_1 = 200;
+int base_2 = 400;
 
-  
+int memory_base_val = base_1;
+int *memory_base = &memory_base_val; 
+
+int memory_base2_val = base_2;
+int *memory_base2 = &memory_base2_val; 
+
+int total_shared_memory = (memory_base - memory_base2);
+
+int  inline_pointer_val = memory_base_val;
+int  *inline_pointer = &inline_pointer_val;
+
+if (inline_pointer_val == memory_base_val){inline_pointer_val--;}
+if (inline_pointer_val == memory_base2_val){inline_pointer_val--;}
+
+void write(){}
+void read(){}
+
 }
