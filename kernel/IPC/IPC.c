ba20c684ce7b84124905224f0cc87479a82e6b33
Justin <justindude057@gmail.com>
2026-06-09 02:24:09 -0400

    Implement multi_ipc function and process management
    Added multi_ipc function to handle multiple IPC processes and modified existing variables to support process management.

--- kernel/IPC/IPC.c
+++ kernel/IPC/IPC.c
@@ -3,8 +3,22 @@
 int main()
 {
 
-int base_1 = 200;
-int base_2 = 400;
+int read_data = 0;
+  
+int write_data = 0;
+  
+int interrupt = 0;
+  
+int processes = 0;
+
+int current_process = 0;
+
+int process1_base1 = 0;
+
+int process2_base2 = 0;
+  
+int base_1 = process1_base1;
+int base_2 = process2_base2;
 
 int memory_base_val = base_1;
 int *memory_base = &memory_base_val; 
@@ -20,7 +34,23 @@ int  *inline_pointer = &inline_pointer_val;
 if (inline_pointer_val == memory_base_val){inline_pointer_val++;}
 if (inline_pointer_val == memory_base2_val){inline_pointer_val--;}
 
-void write(){}
-void read(){}
+void multi_ipc(){
+while(processes > current_process)
+{multi_ipc();
+current_process++;
+if(current_process == processes){current_process = 0;}
+if(interrupt == 1}{current_process--;}
+else {interrupt = 0;}
+}
+multi_ipc();
+
+
+  
+void write(){
+interrupt = 1; //interrupt to write.
+}
+void read(){
+interrupt = 1; //interrupt to read.
+}
 
 }
