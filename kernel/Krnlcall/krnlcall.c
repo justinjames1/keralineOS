a76374f05b04f1923a9fd488764ee063feb17a42
Justin <justindude057@gmail.com>
2026-06-10 00:48:31 -0400

    Implement disk interrupt handler stubs
    Added multiple interrupt handler stubs for disk operations.

--- kernel/Krnlcall/krnlcall.c
+++ kernel/Krnlcall/krnlcall.c
@@ -3,7 +3,28 @@
 int main()
 {
 
-void disk(){
-};
+unsigned int int_type; //define intterupts
+  
+void int_0x01() //disk intterupt
+{}
+void int_0x02() 
+{}
+void int_0x03()
+{}
+void int_0x04()
+{}
+void int_0x05()
+{}
+void int_0x06()
+{}
+void int_0x07()
+{}
+void int_0x08()
+{}
+void int_0x09()
+{}
+void int_0x10()
+{}
+  
   
 }
