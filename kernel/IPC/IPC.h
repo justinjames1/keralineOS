6cd0f0f89d81fc31d55880d497ee81de3e57dbbc
Justin <justindude057@gmail.com>
2026-06-10 03:34:07 -0400

    Update IPC.h

--- kernel/IPC/IPC.h
+++ kernel/IPC/IPC.h
@@ -1,6 +1,16 @@
 #ifndef IPC_H_   /* Include guard */
 #define IPC_H_
 
+
+extern panic_trigger;
+extern read_data;
+extern write_data;
+extern interrupt;
+extern processes;
+extern current_process;
+extern process1_base1;
+extern process2_base2;
+
 void write();
 void read();
 
