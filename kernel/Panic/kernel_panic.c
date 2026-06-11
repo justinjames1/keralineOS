15f20bec0b8112fecf7a2b36d8e365abffee72cd
Justin <justindude057@gmail.com>
2026-06-11 13:02:15 -0400

    kernel_panic.c

--- kernel/Panic/kernel_panic.c
+++ kernel/Panic/kernel_panic.c
@@ -3,7 +3,6 @@
 #include "kernel/Krnlcall/krnlcall.h" //so we can collect data from krnlcall.
 #include "kernel/IPC/IPC.h" // collect data from ipc 
 int main(){
-
 unsigned int error_code;
 void shared_overflow_error_code()
 {error_code = 0x00000001;} //memory region fault. 
