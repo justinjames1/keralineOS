438d4d26b8efea8c700b57e0f40f1325146a4d13
Justin <justindude057@gmail.com>
2026-06-12 13:11:20 -0400

    Add framebuffer address and dimensions to FrameBuffer.h

--- kernel/FrameBuffer/FrameBuffer.h
+++ kernel/FrameBuffer/FrameBuffer.h
@@ -1,9 +1,10 @@
 #ifndef FrameBuffer_h
 #define FrameBuffer_h
 
-extern int color;
-extern int x;
-extern int y;
+extern int framebuffer_addr;
+extern int color1;
+extern int width1;
+extern int height1;
 
 
 #endif
