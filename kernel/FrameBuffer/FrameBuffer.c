db6a9c968b89554fdd87f154d1576b3de5cf27f1
Justin <justindude057@gmail.com>
2026-06-12 13:31:22 -0400

    Update FrameBuffer.c

--- kernel/FrameBuffer/FrameBuffer.c
+++ kernel/FrameBuffer/FrameBuffer.c
@@ -1,2 +1,14 @@
 #include "kernel/FrameBuffer/FrameBuffer.h"
-int main(){}
+int main(){
+struct framebufferdata{
+int *FrameBufferStart;
+int *GpuMemoryAddress;
+int *MemoryArray[8000];
+int *height;
+int *weight;
+};
+struct framebufferdata framebufferdata;
+
+
+  
+}
