71cdf04eb4a68d238ee37ebc2c6b1ed3e4d52c03
Justin <justindude057@gmail.com>
2026-06-13 10:21:30 -0400

    Modify framebufferdata structure for GPU integration
    Updated framebufferdata structure to include GPU memory address and changed MemoryArray to FrameBufferMemoryArray.

--- kernel/FrameBuffer/FrameBuffer.c
+++ kernel/FrameBuffer/FrameBuffer.c
@@ -1,14 +1,18 @@
 #include "kernel/FrameBuffer/FrameBuffer.h"
+#include "kernel/driver_manager/GPU.h"
 int main(){
 struct framebufferdata{
 int *FrameBufferStart;
 int *GpuMemoryAddress;
-int *MemoryArray[8000];
+int *FrameBufferMemoryArray[];
 int *height;
 int *weight;
 };
 struct framebufferdata framebufferdata;
 
+framebufferdata.GpuMemoryAddress = &*GpuMemoryaddress;
+framebufferdata.*FrameBufferMemoryArray[] = &GpuVramSize; //I FUCKING HATE THIS STUPID FUCKING OS CODING ALL OF THIS IS A FUCKING PAIN GOD WHY
+  
 
   
 }
