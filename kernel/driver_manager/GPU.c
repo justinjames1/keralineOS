7a38a41bbdafc8b9d1815c0f68a4960e91a1164f
Justin <justindude057@gmail.com>
2026-06-13 10:15:27 -0400

    Define GpuData structure for GPU memory management
    Added GpuData structure to manage GPU VRAM details.

--- kernel/driver_manager/GPU.c
+++ kernel/driver_manager/GPU.c
@@ -1,4 +1,12 @@
 #include "kernel/driver_manager/GPU.h"
 int main(){
+struct GpuData{
+unsigned int GpuVramMemoryAddress;
+unsigned int GpuVramSize;};
+struct GpuData GpuData;
+*GpuMemoryaddress = &GpuData.GpuVramMemoryAddress;
+GpuVramSize = &GpuData.GpuVramSize;
 
+
+  
 }
