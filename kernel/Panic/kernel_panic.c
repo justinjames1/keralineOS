834d5bf31178a1f94635b02b1877437e7afae470
Justin <justindude057@gmail.com>
2026-06-15 13:51:08 -0400

    Enhance shared_overflow_error_code with error messages
    Refactor shared_overflow_error_code to include detailed error messages.

--- kernel/Panic/kernel_panic.c
+++ kernel/Panic/kernel_panic.c
@@ -3,9 +3,13 @@
 #include "kernel/Krnlcall/krnlcall.h" //so we can collect data from krnlcall.
 #include "kernel/IPC/IPC.h" // collect data from ipc 
 int main(){
+unsigned char error_msg1[512];
+unsigned char error_msg2[512];
 unsigned int error_code;
-void shared_overflow_error_code()
-{error_code = 0x00000001;} //memory region fault. 
+void shared_overflow_error_code(){
+error_code = 0x00000001;
+char error_msg1[512] = "KERNEL PANIC MEMORY REGION FAULT A PROCESSS HAS ESCAPED SHARED MEMORY REGION.";
+char error_msg2[512] = "IN ORDER TO PROTECT SYSTEM PROCESSES AND GENERAL PROCESSES A KERNEL PANIC HAS BEEN ENGAGED.";} //memory region fault. 
  void shared_overflow(){
 if (panic_trigger == 1)
 {shared_overflow_error_code();} 
