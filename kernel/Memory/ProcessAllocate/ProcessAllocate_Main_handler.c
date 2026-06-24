2ca5e43fe17ee629bbeb1e673754f0706dffe762
Justin <justindude057@gmail.com>
2026-06-24 00:11:03 -0400

    Add ProcessAllocate_Main_handler for memory allocation
    Implement memory allocation for processes in a new handler.

---
+++ kernel/Memory/ProcessAllocate/ProcessAllocate_Main_handler.c
@@ -0,0 +1,27 @@
+int main(){
+
+unsigned int ProgramRegionStart;
+
+unsigned int RamBase;
+unsigned int RamRoof;
+struct ram_struct {
+unsigned ram_base;
+unsigned ram_roof;
+}; struct ram_struct ram_struct;
+
+if (RamBase < ProgramRegionStart){RamBase++;}
+
+ram_struct.ram_base = RamBase;
+ram_struct.ram_roof = RamRoof;
+
+int ProgramBase = (ram_struct.ram_base > ProgramBase);
+int ProgramRoof = (ram_struct.ram_Roof < ProgramRoof);
+int ProgramRegion = (ProgramBase - ProgramRoof);
+int ProgramSize = (ProgramRegion);
+
+void Allocate_Process(){
+int allocate[ProgramSize];
+}
+
+
+}
